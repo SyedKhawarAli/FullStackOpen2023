07909b20105ced7c606ea69db823027e4d8478a6
Khawar Ali <khawarali5@gmail.com>
2023-06-19 23:07:03 +0300

    unicafe done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,41 +1,56 @@
+import { useState } from 'react'
 
-const GoodFeedbacks = (props) => {
+const StatisticsLine = (props) => {
   return (
-    <p>
-      good {props.goodFeedbacks}
-    </p>
+    <tr>
+      <td>{props.text}</td><td>{props.value}</td>
+    </tr>
   )
 }
 
-const NeutralFeedbacks = (props) => {
+const Statistics = (props) => {
+  const all = props.goodFeedbacks + props.neutralFeedbacks + props.badFeedbacks
+  const average = (props.goodFeedbacks - props.badFeedbacks) / all
+  const positive = props.goodFeedbacks / all * 100
+  if (all === 0) {
+    return ( <div>No feedback given</div> )
+  }
   return (
-    <p>
-      good {props.neutralFeedbacks}
-    </p>
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={props.goodFeedbacks} />
+        <StatisticsLine text="neutral" value={props.neutralFeedbacks} />
+        <StatisticsLine text="bad" value={props.badFeedbacks} />
+        <StatisticsLine text="all" value={all} />
+        <StatisticsLine text="average" value={average} />
+        <StatisticsLine text="positive" value={positive + " %"} />
+      </tbody>
+    </table>
   )
 }
 
-const BadFeedbacks = (props) => {
-  return (
-    <p>
-      good {props.badFeedbacks}
-    </p>
-  )
-}
+
 
 const App = () => {
-
-  const numberOfGoodFeedbacks = 10
-  const numberOfNeutralFeedbacks = 10
-  const numberOfBadFeedbacks = 10
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div >
       <h1>give feedback </h1>
+      <button onClick={() => setGood(good + 1)}>
+        good
+      </button>
+      <button onClick={() => setNeutral(neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad(bad + 1)}>
+        bad
+      </button>
       <h1>statistics </h1>
-      <GoodFeedbacks goodFeedbacks={numberOfGoodFeedbacks} />
-      <NeutralFeedbacks neutralFeedbacks={numberOfGoodFeedbacks} />
-      <BadFeedbacks badFeedbacks={numberOfGoodFeedbacks} />
+      <Statistics goodFeedbacks={good} neutralFeedbacks={neutral} badFeedbacks={bad} />
     </div>
   );
 }
